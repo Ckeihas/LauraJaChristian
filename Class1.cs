ae79e5d7e6915440b19bfcc08f1d0e13270a7153
Ckeihas <christian.keihas@outlook.com>
2022-06-27 13:58:48 +0300

    Paivamaaran lisäys

--- Class1.cs
+++ Class1.cs
@@ -4,5 +4,22 @@ namespace LauraJaChristianHarkka
 {
     public class Class1
     {
+        public static void Paivamaara()
+        {
+            bool tulevaisuudessa = true;
+
+            Console.WriteLine("Kirjoita pvm: ");
+            DateTime vastaus = Convert.ToDateTime(Console.ReadLine());
+
+            if (vastaus > DateTime.Now)
+            {
+                tulevaisuudessa = true;
+            }
+            else
+            {
+                tulevaisuudessa = false;
+            }
+
+        }
     }
 }
